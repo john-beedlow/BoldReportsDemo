810005d436b4d11821b9ec10cf0e92eae40b09c7
john-beedlow <john.beedlow@dctechusa.com>
2021-12-23 12:43:43 -0500

    Initial commit

---
+++ Data/BoldReportsAPIController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BoldReports.Web.ReportViewer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BlazorReportingTools.Data
+{
+    [Route("api/{controller}/{action}/{id?}")]    
+    public class BoldReportsAPIController : ControllerBase, IReportController
+    {
+        // Report viewer requires a memory cache to store the information of consecutive client request and
+        // have the rendered report viewer information in server.
+        private IMemoryCache _cache;
+
+        // IHostingEnvironment used with sample to get the application data from wwwroot.
+        private IWebHostEnvironment _hostingEnvironment;
+
+        public BoldReportsAPIController(IMemoryCache memoryCache, IWebHostEnvironment hostingEnvironment)
+        {
+            _cache = memoryCache;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        //Get action for getting resources from the report
+        [ActionName("GetResource")]
+        [AcceptVerbs("GET")]
+        // Method will be called from Report Viewer client to get the image src for Image report item.
+        public object GetResource(ReportResource resource)
+        {
+            return ReportHelper.GetResource(resource, this, _cache);
+        }
+
+        // Method will be called to initialize the report information to load the report with ReportHelper for processing.
+        public void OnInitReportOptions(ReportViewerOptions reportOption)
+        {
+            reportOption.ReportModel.ReportServerUrl = "http://10.20.3.22/ReportServer";
+            reportOption.ReportModel.ReportPath = "/GetUsers";
+            reportOption.ReportModel.ReportServerCredential = new System.Net.NetworkCredential("****", "****", "****");
+
+            reportOption.ReportModel.DataSourceCredentials.Add(new BoldReports.Web.DataSourceCredentials("MESDB", "****", "****"));
+        }
+
+        // Method will be called when reported is loaded with internally to start to layout process with ReportHelper.
+        public void OnReportLoaded(ReportViewerOptions reportOption)
+        {
+        }
+
+        [HttpPost]
+        public object PostFormReportAction()
+        {
+            return ReportHelper.ProcessReport(null, this, _cache);
+        }
+
+        // Post action to process the report from server based json parameters and send the result back to the client.
+        [HttpPost]
+        public object PostReportAction([FromBody] Dictionary<string, object> jsonArray)
+        {
+            return ReportHelper.ProcessReport(jsonArray, this, this._cache);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Blazor Reporting Components in Bold Reports
+
+This repository contains example about how to integrate our Bold Reports HTML5/JavaScript [`Report Viewer`](https://www.boldreports.com/embedded/reporting-tools/javascript/report-viewer) and [`Report Designer`](https://www.boldreports.com/embedded/reporting-tools/javascript/report-designer) into [`Blazor`](https://dotnet.microsoft.com/apps/aspnet/web-apps/blazor) application.
+
+## Prerequisites
+
+Before getting started with bold reports, make sure your development environment includes the following requirements.
+
+* [Visual Studio 2019](https://visualstudio.microsoft.com/downloads/) with ASP.NET and Web Development workloads.
+* [.NET Core 3.1](https://dotnet.microsoft.com/download) Framework.
+
+## Running the App
+
+* Open the solution file `BlazorReportingTools.sln` in Visual studio 2019.
+
+* Press `F5` in Visual Studio. Visual Studio will launch your browser for you.
+
+* Navigate to [https://localhost:51885/ReportViewer](https://localhost:51885/ReportViewer) to view a report in Report Viewer.
+
+* Navigate to [https://localhost:51885/ReportDesigner](https://localhost:51885/ReportDesigner) to edit a report in Report Designer.
+
+## Online Demos
+
+Take a look at the Bold Reporting Components live demo [here](https://demos.boldreports.com/home/).
+
+## Documentation
+
+A complete Bold Reports documentation can be found on [Bold Reports Help](https://documentation.boldreports.com/).
+
+## Support and Feedback
+
+* For any other queries, reach our [Bold Reports support team](mailto:support@boldreports.com) or [Feedback portal](https://www.boldreports.com/feedback/).
+
+* To renew the subscription, click [here](https://www.boldreports.com/pricing/on-premise) or contact our sales team at <https://www.boldreports.com/contact>.
+
